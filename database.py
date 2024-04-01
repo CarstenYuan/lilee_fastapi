87270a134455ab2c2e57ae1789890bcad2e66503
袁顥(Haw Yuan) <2311019@iisigroup.com>
2024-04-01 15:50:18 +0800

    add base in order to create tables

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ import configparser
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
-from models.user import Base
-
+from models import Base
 
 config = configparser.ConfigParser()
 config.read('db.config')
@@ -17,6 +16,7 @@ dbname = config['database']['dbname']
 engine_url = f"mysql+pymysql://{username}:{password}@{host}:{port}/{dbname}"
 engine = create_engine(engine_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def init_db():
     if not database_exists(engine.url):
