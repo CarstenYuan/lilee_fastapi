61b16d940f0195f7b2dadd0bd56dad0bb85db7f3
袁顥(Haw Yuan) <2311019@iisigroup.com>
2024-04-01 15:17:09 +0800

    add create db

--- database.py
+++ database.py
@@ -1,7 +1,18 @@
+import configparser
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
-engine = create_engine("mysql+pymysql://<username>:<password>@<host>/<dbname>[?<options>]")
+
+config = configparser.ConfigParser()
+config.read('db.config')
+
+username = config['database']['username']
+password = config['database']['password']
+host = config['database']['host']
+port = config['database']['port']
+dbname = config['database']['dbname']
+
+engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}:{port}/{dbname}")
 
 if not database_exists(engine.url):
     create_database(engine.url)
