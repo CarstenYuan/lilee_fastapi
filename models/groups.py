22cae574502b36f4b903ca49e10a9631d88dcff0
袁顥(Haw Yuan) <2311019@iisigroup.com>
2024-04-01 16:06:33 +0800

    Add relationship between Users and Groups

--- models/groups.py
+++ models/groups.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from .base import Base
 
 
@@ -6,3 +7,5 @@ class Groups(Base):
     __tablename__ = "groups"
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
+
+    users = relationship("Users", back_populates="group")
--- models/users.py
+++ models/users.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from .base import Base
 
 
@@ -7,3 +8,5 @@ class Users(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
     group_id = Column(Integer, ForeignKey('groups.id'))
+
+    group = relationship("Groups", back_populates="users")
