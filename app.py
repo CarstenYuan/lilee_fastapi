f82d6a897659e012fa805f4f0e0481ffb11cbf56
袁顥(Haw Yuan) <2311019@iisigroup.com>
2024-04-01 15:36:33 +0800

    add init_db

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from apis import router
 from uvicorn import run as uvicorn_run
 
+from database import init_db
+
 
 app = FastAPI(title='LiLee Web Server APIs', docs_url='/swagger')
 app.include_router(router)
 
 
 if __name__ == '__main__':
+    init_db()
     uvicorn_run(app, host='127.0.0.1', port=9000, workers=1, reload=False)
