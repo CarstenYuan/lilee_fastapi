b1f3d76cc3d861f5aeed1a5e1c1ef7fdd52d8320
袁顥(Haw Yuan) <2311019@iisigroup.com>
2024-04-03 19:07:08 +0800

    move app back to the top and change uvicorn_run's content

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 import argparse
+from apis import router
 from fastapi import FastAPI
 from uvicorn import run as uvicorn_run
 
-
+app = FastAPI(title='LiLee Web Server APIs', docs_url='/swagger')
+app.include_router(router)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -13,10 +15,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     os.environ["DB_CONFIG_FILE"] = args.config
 
-    # apis will import database and create/use MySQLDB,
-    # so move down here in order to get the desired file path
-    from apis import router
-    
-    app = FastAPI(title='LiLee Web Server APIs', docs_url='/swagger')
-    app.include_router(router)
-    uvicorn_run(app, host='127.0.0.1', port=9000, workers=1, reload=False)
+    # uvicorn_run(app, host='127.0.0.1', port=9000, workers=1, reload=False)
+    uvicorn_run('app:app', host='127.0.0.1', port=9000, reload=True)
