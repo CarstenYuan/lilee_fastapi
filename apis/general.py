593ebfcd3a0949e39b2439693e8a7f3b092c7c6a
袁顥(Haw Yuan) <2311019@iisigroup.com>
2024-04-03 19:06:14 +0800

    move object MySQLDB into each function

--- apis/general.py
+++ apis/general.py
@@ -2,10 +2,8 @@ from database import MySQLDB
 from models import Users
 
 
-db_manager = MySQLDB()
-
-
 def add_item(model_class, **kwargs):
+    db_manager = MySQLDB()
     db_session = db_manager.SessionLocal()
     try:
         item = model_class(**kwargs)
@@ -21,6 +19,7 @@ def add_item(model_class, **kwargs):
 
 
 def delete_item(model_class, item_id):
+    db_manager = MySQLDB()
     db_session = db_manager.SessionLocal()
     try:
         item = db_session.query(model_class).filter(model_class.id == item_id).one_or_none()
@@ -38,12 +37,14 @@ def delete_item(model_class, item_id):
 
 
 def can_delete_group(group_id: int) -> bool:
+    db_manager = MySQLDB()
     db_session = db_manager.SessionLocal()
     users_count = db_session.query(Users).filter(Users.group_id == group_id).count()
     return users_count == 0
 
 
 def get_single_item(model_class, item_id):
+    db_manager = MySQLDB()
     db_session = db_manager.SessionLocal()
     try:
         item = db_session.query(model_class).filter(model_class.id == item_id).one_or_none()
@@ -55,6 +56,7 @@ def get_single_item(model_class, item_id):
 
 
 def get_all_items(model_class, filter: str = None):
+    db_manager = MySQLDB()
     db_session = db_manager.SessionLocal()
     try:
         query = db_session.query(model_class)
