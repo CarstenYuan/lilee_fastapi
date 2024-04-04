076c7bec8e5f7d0e0a5a58f3e032e2ce2ce8099c
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-05 04:10:19 +0800

    remove useless functions and assign db.config for Docker use, will design a more dynamical assign method in the future

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import os
 import configparser
 
-from models import Base, Groups, Users
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
@@ -9,7 +8,8 @@ from sqlalchemy_utils import database_exists, create_database
 
 class MySQLDB:
     def __init__(self):
-        config_file = os.getenv("DB_CONFIG_FILE", "./db.config")
+        # TODO: Dynamically assign config file to DB, ex: debug mode
+        config_file = "./db.config"
         config = configparser.ConfigParser()
         config.read(config_file)
 
@@ -22,19 +22,3 @@ class MySQLDB:
         self.engine_url = f"mysql+pymysql://{self.username}:{self.password}@{self.host}:{self.port}/{self.dbname}"
         self.engine = create_engine(self.engine_url)
         self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
-
-        # check if db exists
-        if not database_exists(self.engine.url):
-            create_database(self.engine.url)
-            # create users and groups tables
-            self.create_tables() 
-        
-    def get_db(self):
-        db = self.SessionLocal()
-        try:
-            yield db
-        finally:
-            db.close()
-
-    def create_tables(self):
-        Base.metadata.create_all(bind=self.engine)
