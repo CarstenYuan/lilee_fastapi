7cd8ab7aa5a3bee85737ceb0d721672cc1132e2f
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-06 00:03:46 +0800

    rename the url to start with a lowercase char

--- apis/groups.py
+++ apis/groups.py
@@ -19,7 +19,7 @@ def add_group(name: str):
     return {"item_type": "Group", "name": group.name, "id": group.id}
 
 
-@groups_statistic_router.delete("/DeleteGroup", tags=groups_tag)
+@groups_statistic_router.delete("/deleteGroup", tags=groups_tag)
 def delete_group(id: int):
     if not can_delete_group(id):
         raise HTTPException(status_code=400, detail="Group cannot be deleted because it has members.")
@@ -30,7 +30,7 @@ def delete_group(id: int):
     raise HTTPException(status_code=404, detail=f"Group with id {id} does not exist.")
 
 
-@groups_statistic_router.get("/GetSingleGroup/{group_id}", tags=groups_tag)
+@groups_statistic_router.get("/getSingleGroup/{group_id}", tags=groups_tag)
 def get_single_group(id: int):
     group = get_single_item(Groups, id)
     if group:
@@ -38,13 +38,13 @@ def get_single_group(id: int):
     raise HTTPException(status_code=404, detail=f"Group with id {id} does not exist.")
 
 
-@groups_statistic_router.get("/GetAllGroups", tags=groups_tag)
+@groups_statistic_router.get("/getAllGroups", tags=groups_tag)
 def get_all_groups():
     groups = get_all_items(Groups)
     return groups
 
 
-@groups_statistic_router.patch("/UpdateIsGroupActivate", tags=groups_tag)
+@groups_statistic_router.patch("/updateIsGroupActivate", tags=groups_tag)
 def update_is_group_activate(id: int, is_activate: bool):
     if (not is_activate) and (not can_delete_group(id)):
         raise HTTPException(status_code=400, detail="Group cannot be deactivated because it has members.")
