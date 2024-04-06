03fbe39d67908855fab88b24805e32472ac0339b
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-06 15:32:36 +0800

    add CORS

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from apis import router
 from fastapi import FastAPI
 from uvicorn import run as uvicorn_run
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title='LiLee Web Server APIs', docs_url='/swagger')
 app.include_router(router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == '__main__':
