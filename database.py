59cd537dafd84c2246ec17319fd36b13ccd2b40a
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-12 11:07:48 +0800

    remove MySQLDB class, create a session directly, and add get_db function

--- database.py
+++ database.py
@@ -1,24 +1,17 @@
-import os
-import configparser
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, create_database
 
 
-class MySQLDB:
-    def __init__(self):
-        # TODO: Dynamically assign config file to DB, ex: debug mode
-        config_file = "./db.config"
-        config = configparser.ConfigParser()
-        config.read(config_file)
+# engine_url = "mysql+pymysql://root:1qaz2wsx@localhost:3306/lilee"  # for local tests
+engine_url = "mysql+pymysql://root:1qaz2wsx@mysqlDB:3306/lilee"
 
-        self.username = config['database']['username']
-        self.password = config['database']['password']
-        self.host = config['database']['host']
-        self.port = config['database']['port']
-        self.dbname = config['database']['dbname']
+engine = create_engine(engine_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 
-        self.engine_url = f"mysql+pymysql://{self.username}:{self.password}@{self.host}:{self.port}/{self.dbname}"
-        self.engine = create_engine(self.engine_url)
-        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine, expire_on_commit=False)
+
+def get_db():
+    db_session = SessionLocal()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
