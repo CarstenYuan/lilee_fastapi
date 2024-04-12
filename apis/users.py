fe51a3f4d300f0eb9add0ebee5a4ad73ee208483
Haw Yuan <hawyuan1lv@gmail.com>
2024-04-12 11:12:10 +0800

    add session to functions we use, remove validator in InfoRequest

--- apis/users.py
+++ apis/users.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException, Query, Body
-from pydantic import BaseModel, Field, validator
+from fastapi import APIRouter, HTTPException, Query, Body, Depends
+from pydantic import BaseModel, Field
 from typing import Optional
 from models import Users, Groups
+from sqlalchemy.orm import Session
+from database import get_db
 
 from apis.general import (
                             add_item,
@@ -22,62 +24,50 @@ class AddUserInfoRequest(BaseModel):
     name: str = Field(description="The new name of the user.")
     group_id: Optional[int] = Field(None, description="The new group ID of the user.")
 
-    @validator("group_id")
-    def is_group_id_valid(cls, g_id):
-        if g_id == 0:
-            g_id = None
-        if (g_id is not None) and (not can_join_group(g_id)):
-            raise HTTPException(status_code=400, detail="You cannot join a deactivated group.")
-        return g_id
-
 
 class UpdateUserInfoRequest(BaseModel):
     name: str = Field(None, description="The updated name of the user.")
     group_id: Optional[int] = Field(None, description="The updated group ID of the user.")
     is_activate: bool = Field(None, description="The updated activation status of the user.")
 
-    @validator("group_id")
-    def is_group_id_valid(cls, g_id):
-        if g_id == 0:
-            g_id = None
-        if (g_id not in (None, -1)) and (not can_join_group(g_id)):
-            raise HTTPException(status_code=400, detail="You cannot join a deactivated group.")
-        return g_id
-
 
 @users_statistic_router.post("/addUser", tags=users_tag)
-def add_user(add_request: AddUserInfoRequest = Body(...)):
+def add_user(add_request: AddUserInfoRequest = Body(...), db_session: Session = Depends(get_db)):
     add_data = add_request.dict()
-    user = add_item(Users, name=add_data['name'], group_id=add_data['group_id'])
+    if add_data['group_id'] == 0:
+        add_data['group_id'] = None
+    if add_data['group_id'] and not can_join_group(add_data['group_id'], db_session):
+        raise HTTPException(status_code=400, detail="You cannot join a deactivated group.")
+    user = add_item(Users, db_session, name=add_data['name'], group_id=add_data['group_id'])
     return user
 
 
 @users_statistic_router.delete("/deleteUser/{id}", tags=users_tag)
-def delete_user(id: int):
-    user = delete_item(Users, id)
+def delete_user(id: int, db_session: Session = Depends(get_db)):
+    user = delete_item(Users, id, db_session)
     if user:
         return user
     raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
 
 
 @users_statistic_router.get("/getSingleUser/{id}", tags=users_tag)
-def get_single_user(id: int):
-    user = get_single_item(Users, id)
+def get_single_user(id: int, db_session: Session = Depends(get_db)):
+    user = get_single_item(Users, id, db_session)
     if user:
         return user
     raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
 
 
 @users_statistic_router.get("/getAllUsers", tags=users_tag)
-def get_all_users(filter: Optional[str] = Query(None)):
-    users = get_all_items(Users, filter=filter)
+def get_all_users(filter: Optional[str] = Query(None), db_session: Session = Depends(get_db)):
+    users = get_all_items(Users, db_session, filter=filter)
     users_list = []
     for user in users:
         curr_id = user.id
 
         curr_name = user.name
         curr_group_id = user.group_id
-        group = get_single_item(Groups, curr_group_id)
+        group = get_single_item(Groups, curr_group_id, db_session)
         curr_group_name = group.name if group else None
 
         curr_creator = user.creator
@@ -104,17 +94,23 @@ def get_all_users(filter: Optional[str] = Query(None)):
 
 
 @users_statistic_router.patch("/updateIsUserActivate/{id}", tags=users_tag)
-def update_is_user_activate(id: int, is_activate: bool):
-    user = update_is_activate(Users, id, is_activate)
+def update_is_user_activate(id: int, is_activate: bool, db_session: Session = Depends(get_db)):
+    user = update_is_activate(Users, id, is_activate, db_session)
     if user:
         return user
     raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
 
 
 @users_statistic_router.put("/updateUserInfo/{id}", tags=users_tag)
-def update_user_info(id: int, update_request: UpdateUserInfoRequest = Body(...)):
+def update_user_info(id: int, update_request: UpdateUserInfoRequest = Body(...), db_session: Session = Depends(get_db)):
     update_data = update_request.dict()
-    user = update_items(Users, id, update_data)
+
+    if update_data['group_id'] == 0:
+        update_data['group_id'] = None
+    if update_data['group_id'] and not can_join_group(update_data['group_id'], db_session):
+        raise HTTPException(status_code=400, detail="You cannot join a deactivated group.")
+
+    user = update_items(Users, id, update_data, db_session)
     if user:
         return user
     raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
